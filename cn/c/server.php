6304e03ddfc25a0ee9b06b1fb714174942b3131c
tool321 <tool321@qq.com>
2016-03-31 22:17:01 +0800

    first php page for test

---
+++ cn/c/server.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="zh">
+
+<head>
+	<!-- Required meta tags always come first -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<base target="_blank">
+	<!--用户可方便地通过 Windows 启动磁贴来查找和固定您的网页-->
+	<meta name="msapplication-TileColor" content="#123456" />
+	<meta name="msapplication-square150x150logo" content="square.png" />
+	<title>服务器工具--Tool321</title>
+	<link href="/css/style_css.css" rel="stylesheet" type="text/css">
+	<meta name="keywords" content="服务器，生产力,效率,工具,导航,正版，云端，在线，入口，different,productivity,efficiency,tool,navigation,SAAS,online，entrance">
+	<meta name="author" content="马背岭，Horseback">
+	<meta name="description" content="服务器工具，由从事质量十年的一个专业职业人创建，主要是为了解决自己频繁试用各种最新工具而建，目的在于减少自己的搜索量，直达工具在线使用页面或者下载页面。收集了目前各个领域里的最好工具，方便自己的同时，方便所有互联网的使用者" />
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.css">
+
+</head>
+
+<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-light bg-faded">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<ul class="nav navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Features</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Priing</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About</a>
+				</li>
+			</ul>
+			<form class="form-inline pull-xs-right">
+				<input class="form-control" type="text" placeholder="Search">
+				<button class="btn btn-success-outline" type="submit">Search</button>
+			</form>
+		</nav>
+
+		<ol class="breadcrumb" style="margin-bottom: 5px;">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Library</a></li>
+			<li class="active">Data</li>
+		</ol>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-8">
+
+				<table class="table table-reflow">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Table heading</th>
+							<th>Table heading</th>
+							<th>Table heading</th>
+							<th>Table heading</th>
+							<th>Table heading</th>
+							<th>Table heading</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+						</tr>
+						<tr>
+							<th scope="row">2</th>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+						</tr>
+						<tr>
+							<th scope="row">3</th>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+							<td>Table cell</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<div class="jumbotron jumbotron-fluid">
+					<div class="container">
+						<h1 class="display-3">Fluid jumbotron</h1>
+						<p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="col-xs-6 col-md-4">
+				<div class="card">
+					<div class="card-header">New tools online</div>
+					<div class="list-group">
+						<a href="#" class="list-group-item active">
+							<h4 class="list-group-item-heading">List group item heading</h4>
+							<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+						</a>
+						<a href="#" class="list-group-item">
+							<h4 class="list-group-item-heading">List group item heading</h4>
+							<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+						</a>
+						<a href="#" class="list-group-item">
+							<h4 class="list-group-item-heading">List group item heading</h4>
+							<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+						</a>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="card-header">New tools online</div>
+					<div class="list-group">
+						<a href="#" class="list-group-item active">
+    Cras justo odio
+  </a>
+						<a href="#" class="list-group-item">nginx</a>
+						<a href="#" class="list-group-item"></a>
+						<a href="#" class="list-group-item"></a>
+						<a href="#" class="list-group-item"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- jQuery first, then Bootstrap JS. -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
+
+</html>
