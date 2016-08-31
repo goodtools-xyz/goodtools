f92fd486ed8ceca7ec6231db025dc46aab1e2237
tool321 <tool321@qq.com>
2016-08-31 14:04:05 +0800

    item adjust

--- cn/c/server.php
+++ cn/c/server.php
@@ -2,176 +2,112 @@
 <html lang="zh">
 
 <head>
-	<!-- Required meta tags always come first -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<base target="_blank">
-	<!--用户可方便地通过 Windows 启动磁贴来查找和固定您的网页-->
-	<meta name="msapplication-TileColor" content="#123456" />
-	<meta name="msapplication-square150x150logo" content="square.png" />
-	<title>服务器工具--Tool321</title>
-	<link href="/css/style_css.css" rel="stylesheet" type="text/css">
-	<meta name="keywords" content="服务器，生产力,效率,工具,导航,正版，云端，在线，入口，different,productivity,efficiency,tool,navigation,SAAS,online，entrance">
-	<meta name="author" content="马背岭，Horseback">
-	<meta name="description" content="服务器工具，由从事质量十年的一个专业职业人创建，主要是为了解决自己频繁试用各种最新工具而建，目的在于减少自己的搜索量，直达工具在线使用页面或者下载页面。收集了目前各个领域里的最好工具，方便自己的同时，方便所有互联网的使用者" />
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.css">
-
-</head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <base target="_blank">
+    <!--用户可方便地通过 Windows 启动磁贴来查找和固定您的网页-->
+    <meta name="msapplication-TileColor" content="#123456" />
+    <meta name="msapplication-square150x150logo" content="square.png" />
+    <title>云服务器</title>
+    <link href="/css/style_css.css" rel="stylesheet" type="text/css">
+    <meta name="keywords" content="服务器，生产力,效率,工具,导航,正版，云端，在线，入口，different,productivity,efficiency,tool,navigation,SAAS,online，entrance">
+    <meta name="author" content="马背岭，Horseback">
+    <meta name="description" content="服务器工具，由从事质量十年的一个专业职业人创建，主要是为了解决自己频繁试用各种最新工具而建，目的在于减少自己的搜索量，直达工具在线使用页面或者下载页面。收集了目前各个领域里的最好工具，方便自己的同时，方便所有互联网的使用者" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.0.0/normalize.css"> </head>
 
 <body>
-	<div class="container-fluid">
-		<nav class="navbar navbar-light bg-faded">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<ul class="nav navbar-nav">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Features</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Priing</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">About</a>
-				</li>
-			</ul>
-			<form class="form-inline pull-xs-right">
-				<input class="form-control" type="text" placeholder="Search">
-				<button class="btn btn-success-outline" type="submit">Search</button>
-			</form>
-		</nav>
-
-		<ol class="breadcrumb" style="margin-bottom: 5px;">
-			<li><a href="#">Home</a></li>
-			<li><a href="#">Library</a></li>
-			<li class="active">Data</li>
-		</ol>
-
-		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-8">
-
-				<table class="table table-reflow">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Table heading</th>
-							<th>Table heading</th>
-							<th>Table heading</th>
-							<th>Table heading</th>
-							<th>Table heading</th>
-							<th>Table heading</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<th scope="row">1</th>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-						</tr>
-						<tr>
-							<th scope="row">2</th>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-						</tr>
-						<tr>
-							<th scope="row">3</th>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-							<td>Table cell</td>
-						</tr>
-					</tbody>
-				</table>
-
-				<div class="jumbotron jumbotron-fluid">
-					<div class="container">
-						<h1 class="display-3">Fluid jumbotron</h1>
-						<p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-					</div>
-				</div>
-
-			</div>
-
-			<div class="col-xs-6 col-md-4">
-				<div class="card">
-					<div class="card-header">New tools online</div>
-					<div class="list-group">
-						<a href="#" class="list-group-item active">
-							<h4 class="list-group-item-heading">List group item heading</h4>
-							<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-						</a>
-						<a href="#" class="list-group-item">
-							<h4 class="list-group-item-heading">List group item heading</h4>
-							<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-						</a>
-						<a href="#" class="list-group-item">
-							<h4 class="list-group-item-heading">List group item heading</h4>
-							<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-						</a>
-					</div>
-				</div>
-
-				<div class="card">
-					<div class="card-header">New tools online</div>
-					<div class="list-group">
-						<a href="#" class="list-group-item active">
-    Cras justo odio
-  </a>
-						<a href="#" class="list-group-item">nginx</a>
-						<a href="#" class="list-group-item"></a>
-						<a href="#" class="list-group-item"></a>
-						<a href="#" class="list-group-item"></a>
-					</div>
-				</div>
-			</div>
-		</div>
-		 <footer>
-        <!--QUALIFICAITON INFORMATION-->
-
-        <div id="footer">
-            <ul>
-                <li>&copy;Copyright Reserved 引用请注明来自tool321.com</li>
-                <li>Author:Horseback</li>
-                <li>QQ:1415948426</li>
-                <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=qNzHx8Sbmpno2dmGy8fF" style="text-decoration:none;">意见反馈</a></li>
-                <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=qNzHx8Sbmpno2dmGy8fF" style="text-decoration:none;">提交登录或者定制</a></li>
-                <li>
-                    <!-- #BeginDate format:IS1m -->2016-03-03 14:36
-                    <!-- #EndDate -->
-                </li>
-                <li>
-                    <!--CNZZ统计-->
-                    <script type="text/javascript">
-                        var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-                        document.write(unescape("%3Cspan id='cnzz_stat_icon_1257102666'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1257102666%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
-                    </script>
-                </li>
+    <div class="container-fluid">
+        <nav class="navbar navbar-light bg-faded"> <a class="navbar-brand" href="#">Navbar</a>
+            <ul class="nav navbar-nav">
+                <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Features</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Priing</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
             </ul>
-            <ul>
-                <a href="http://www.miibeian.gov.cn" target="_blank">粤ICP备15117589号</a>
-            </ul>
-        </div>
-    </footer>
-	</div>
-
-	<!-- jQuery first, then Bootstrap JS. -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+            <form class="form-inline pull-xs-right">
+                <input class="form-control" type="text" placeholder="Search">
+                <button class="btn btn-success-outline" type="submit">Search</button>
+            </form>
+        </nav>
+        <ol class="breadcrumb" style="margin-bottom: 5px;">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Library</a></li>
+            <li class="active">Data</li>
+        </ol>
+        <table>
+            <!--   tr>th*2+td*5     -->
+            <tbody>
+                <tr>
+                    <th><i class="fa fa-cloud-download fa-lg" aria-hidden="true"></i>&nbsp;云服务器</th>
+                    <th><a href="http://www.windowsazure.cn" target="_blank">Windows Azure</a></th>
+                    <td><a href="http://aws.amazon.com" target="_blank">亚马逊云</a></td>
+                    <td><a href="http://www.aliyun.com/" target="_blank">阿里云</a></td>
+                    <td><a href="https://bce.baidu.com" target="_blank">百度开放云</a></td>
+                    <td><a href="http://www.qcloud.com/" target="_blank">腾讯云</a></td>
+                    <td><a href="http://www.hwclouds.com/" target="_blank">华为企业云</a></td>
+                </tr>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <td><a href="http://www.west.cn/">西部数码</a></td>
+                    <td><a href="https://www.qingcloud.com/">青云</a></td>
+                    <td><a href="http://www.qiniu.com" target="_blank">七牛</a></td>
+                    <td><a href="http://www.ksyun.com/">金山云</a></td>
+                    <td><a href="http://cloud.google.com">谷歌云</a></td>
+                </tr>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <td><a href="http://www.chinanetcenter.com/">网宿</a></td>
+                    <td><a href="https://www.ucloud.cn/">Ucloud</a></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th><i class="fa fa-server fa-lg" aria-hidden="true"></i><a href="/cn/c/server.php"> 服务器</a></th>
+                    <th><a href="https://www.microsoft.com/en-us/server-cloud/locale.aspx" target="_blank">Windows Server</a></th>
+                    <td><a href="http://www.redhat.com/zh/technologies/all-products" target="_blank">RedHat</a></td>
+                    <td><a href="http://www.apple.com/cn/osx/server/servers-made-easy/">MAC OS Server</a></td>
+                    <td><a href="https://www.centos.org" target="_blank">CentOS</a></td>
+                    <td><a href="http://www.cs2c.com.cn/" target="_blank" class="cellhightlight">中标麒麟</a></td>
+                    <td><a href="https://www.clearos.com" target="_blank">Clear OS</a></td>
+                </tr>
+            </tbody>
+        </table>
+        <footer>
+            <!--QUALIFICAITON INFORMATION-->
+            <div id="footer">
+                <ul>
+                    <li>&copy;Copyright Reserved 引用请注明来自tool321.com</li>
+                    <li>Author:Horseback</li>
+                    <li>QQ:1415948426</li>
+                    <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=qNzHx8Sbmpno2dmGy8fF" style="text-decoration:none;">意见反馈</a></li>
+                    <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=qNzHx8Sbmpno2dmGy8fF" style="text-decoration:none;">提交登录或者定制</a></li>
+                    <li>
+                        <!-- #BeginDate format:IS1m -->2016-03-03 14:36
+                        <!-- #EndDate -->
+                    </li>
+                    <li>
+                        <!--CNZZ统计-->
+                        <script type="text/javascript">
+                            var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+                            document.write(unescape("%3Cspan id='cnzz_stat_icon_1257102666'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1257102666%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
+                        </script>
+                    </li>
+                </ul>
+                <ul> <a href="http://www.miibeian.gov.cn" target="_blank">粤ICP备15117589号</a> </ul>
+            </div>
+        </footer>
+    </div>
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
 </body>
 
 </html>
