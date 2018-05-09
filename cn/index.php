e5e41e4eccc6fe87a8ef0c429975a6e40f6f4644
sunianfu <tool321@qq.com>
2018-05-09 09:07:41 +0800

    0.99 build php index

---
+++ cn/index.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="zh">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="职业导航，工具导航，行业导航，生产力,效率,工具,自媒体导航,导航,正版，云端，在线，微信，小程序，企业应用商店，different,productivity,efficiency,tool,navigation,SAAS,online,cloud,自媒体">
+    <meta name="author" content="马背岭，Horseback">
+    <meta name="description" content="由从事质量十年的一个专业职业人创建，主要是为了解决自己频繁试用各种最新工具而建，目的在于减少自己的搜索量，直达工具在线使用页面或者下载页面。收集了目前各个领域里的最好工具，方便自己的同时，方便所有互联网的使用者">
+    <base target="_blank">
+    <!--用户可方便地通过 Windows 启动磁贴来查找和固定您的网页-->
+    <meta name="msapplication-square150x150logo" content="square.png" />
+    <title>Tool321_不可错过任何一个工具</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+       <link rel="stylesheet" href="/css/nav.css">
+
+    
+</head>
+
+<body>
+    <?php 
+    include "nav.html"
+ ?>
+                                        <table >
+                                            <tbody>
+                                                <tr>
+                                                    <td id="tabs1">互联网入口</td>
+                                                </tr>
+                                                <tr>
+                                                    <th><a href="http://www.capterra.com/categories">工具类别</a></th>
+
+                                                </tr>
+                                                <tr>
+                                                    <th><i class="fas fa-search"></i> <a href="/cn/c/searchengine.html">搜索</a></th>
+                                                    <td><a href="http://www.baidu.com" target="_blank">百度</a></td>
+                                                    <td><a href="http://www.haosou.com" target="_blank">360搜索</a></td>
+                                                    <td class="success"><a href="http://www.bing.com" target="_blank">Bing 必应</a></td>
+                                                    <td><a href="http://www.taobao.com" target="_blank">淘宝</a></td>
+                                                    <td><a href="http://www.jd.com" target="_blank">京东</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th></th>
+                                                    <td><a href="http://xueshu.baidu.com" target="_blank">百度学术</a></td>
+                                                    <td><a href="http://www.sobaidupan.com/" target="_blank">百度网盘搜</a></td>
+                                                    <td><a href="http://weixin.sogou.com" target="_blank">搜狗微信</a></td>
+                                                    <td><a href="http://www.amazon.cn" target="_blank">亚马逊</a>&nbsp;<a href="https://www.amazon.com">En</a></td>
+                                                    <td><a href="http://www.douban.com" target="_blank">豆瓣</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th></th>
+                                                    <td><a href="http://www.dangdang.com" target="_blank">当当</a></td>
+                                                    <td><a href="http://www.alibaba.cn" target="_blank">阿里巴巴</a></td>
+                                                    <td><a href="http://www.pss-system.gov.cn" target="_blank">专利检索</a></td>
+                                                    <td><a href="http://sbj.saic.gov.cn/sbcx/" target="_blank">商标查询</a></td>
+                                                    <td><a href="https://www.yandex.ru" target="_blank">俄Yandex</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th><i class="fas fa-globe"></i> <a href="/cn/c/surfing.html">上网冲浪</a></th>
+
+                                                    <td><a href="http://www.36zhen.com/" target="_blank">36镇</a></td>
+                                                    <td><a href="/cn/c/surfing.html" target="_blank">浏览器</a></td>
+                                                    <td><a href="/cn/c/surfing.html">浏览器开发者版</a></td>
+                                                    <td><a href="http://hao.360.cn/" target="_blank">360导航</a></td>
+                                                    <td><a href="https://adblockplus.org/zh_CN/">Adblock广告拦截</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th><i class="fab fa-wordpress"></i><a href="/cn/c/mp.html" target="_blank">自媒体</a>
+                                                    </th>
+                                                    <td><a href="https://mp.toutiao.com/" target="_blank">头条号</a></td>
+                                                    <td><a href="http://weibo.com" target="_blank">新浪微博</a></td>
+                                                    <td><a href="https://om.qq.com/" target="_blank">企鹅媒体平台</a></td>
+                                                    <td><a href="http://qzone.qq.com" target="_blank">QQ空间</a></td>
+                                                    <td><a href="http://www.jianshu.com" target="_blank" class="cellhightlight">简书</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th><img src="http://www.360.cn/favicon.ico" alt="windows" width="16" height="16"><a href="/cn/c/safety.html">&nbsp;安全工具</a></th>
+
+                                                    <td><a href="http://www.360.cn/weishi/">360安全卫士</a></td>
+                                                    <td><a href="http://www.sd.360.cn">360杀毒</a></td>
+                                                    <td>360<a href="http://www.360.cn/jijiuxiang">急救箱</a> <a href="http://jijiupan.360.cn">急救盘</a></td>
+                                                    <td><a href="http://anquan.baidu.com">百度安全</a></td>
+                                                    <td><a href="http://guanjia.qq.com">腾讯电脑管家</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th><i class="fas fa-download"></i> <a href="/cn/c/download.html">下载中心</a></th>
+
+                                                    <td><a href="http://soft.hao123.com/" target="_blank">Hao123下载</a></td>
+                                                    <td><a href="http://rj.baidu.com">百度软件中心</a></td>
+                                                    <td><a href="http://lixian.vip.xunlei.com" target="_blank">迅雷离线下载</a></td>
+                                                    <td><a href="https://www.microsoft.com/zh-cn/download/" target="_blank">微软下载中心</a></td>
+                                                    <td>文件<a href="https://www.toolsley.com/hash.html" target="_blank">校验</a> <a href="https://1024tools.com/hash" target="_blank">计算</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th><i class="fas fa-shopping-cart"></i> <a href="/cn/c/mall.html">商城</a></th>
+                                                    <td><a href="http://www.jd.com" target="_blank">京东</a></td>
+                                                    <td><a href="http://www.tmall.com" target="_blank">天猫</a></td>
+                                                    <td><a href="http://www.mi.com" target="_blank">小米</a></td>
+                                                    <td><a href="http://www.kaola.com">考拉海购</a></td>
+                                                    <td><a href="http://www.maigoo.com/">买购网</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th><i class="fas fa-shopping-bag"></i>&nbsp;<a href="/cn/c/appstore.html">应用商店</a></th>
+
+                                                    <td><a href="http://shouji.baidu.com/" target="_blank">百度手机助手</a></td>
+                                                    <td><a href="http://www.questmobile.com.cn/">QuestMobile</a></td>
+                                                    <td><a href="https://www.microsoft.com/zh-cn/store/apps/windows/">微软应用商店</a>
+                                                    </td>
+                                                    <td><a href="https://minapp.com">小程序商店</a></td>
+                                                    <td><a href="http://3li3.com/">三立三</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th><i class="fas fa-credit-card"></i> <a href="/cn/c/pay.html">钱包</a></th>
+                                                    <td><a href="https://www.alipay.com/" target="_blank">支付宝</a></td>
+                                                    <td><a href="https://pay.weixin.qq.com" target="_blank">微信支付</a></td>
+                                                    <td><a href="http://www.hao123.com/bank" target="_blank">Hao123银行</a></td>
+                                                    <td><a href="https://gupiao.baidu.com/" target="_blank">百度股市通</a></td>
+                                                    <td><a href="http://www.wacai.com/" target="_blank">挖财</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <th><i class="fas fa-microphone" aria-hidden="true"></i>&nbsp;<a href="/cn/c/voice.html">语音助手</a></th>
+
+                                                    <td><a href="http://fd.zaih.com/fenda">分答</a></td>
+                                                    <td><a href="http://luyin.voicecloud.cn" target="_blank">讯飞录音宝</a></td>
+                                                    <td><a href="http://www.tingdao.me" target="_blank">听道</a></td>
+                                                    <td><a href="http://duer.baidu.com" target="_blank">度秘</a></td>
+                                                    <td><a href="http://yy.sogou.com" target="_blank">搜狗语音</a></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                   
+</body>
+
+</html>
